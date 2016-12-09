13a401881c60f873fd4a2450d03585154f9d1744
arthyom <allpipiasaaa@hotmail.com>
2016-12-08 21:22:34 -0600

    mejorando el script

--- holaMundoTravis.rb
+++ holaMundoTravis.rb
@@ -1,6 +1,10 @@
-numSaludo = gets.chomp.to_i
+entrada = [1,2,3,4,5,6,1.2,'e','a']
 
-#### imprimir un saludo una cantidad n de veces
-numSaludo.times do 
-	puts "hola Travis CI desde ruby"
+entrada.each do | elemnt |
+	numSaludo = elemnt
+
+	#### imprimir un saludo una cantidad n de veces
+	numSaludo.times do 
+		puts "hola Travis CI desde ruby"
+	end
 end
